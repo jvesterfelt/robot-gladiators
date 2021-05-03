22caf7c779d0ed3171509791fb52e7c0ae66ebb5
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-05-03 13:22:43 -0600

    Added name variable and logging.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// This will do math and log 20
+console.log(10 + 10);
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+
+// fight();
