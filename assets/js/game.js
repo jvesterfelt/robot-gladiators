60edb245483094f28a87739762689539ae71c208
JVesterfelt <81572838+jvesterfelt@users.noreply.github.com>
2021-10-25 18:34:06 -0600

    Added logic to repeat through all enemies.

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,21 +3,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth === 0) {
-    console.log("This will not run.");
-} else {
-    console.log("This will run instead.");
-}
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -66,7 +57,14 @@ var fight = function() {
         window.alert("You need to choose a valid option. Please try again!");
     }
 
+    /*Game States
+    "Win" - Player robot has defeated all enemy-robots
+    * Fight all enemy-robots
+    * Defeat each enemy-robot
+    * "Lose" - Player robot's health is zero or less*/
 
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
