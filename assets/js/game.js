3622eb9ffda64128c447422f07511a70fd779bec
JVesterfelt <81572838+jvesterfelt@users.noreply.github.com>
2021-10-31 14:47:35 -0600

    Added some start game and end game logic, as well as resetting user stats.

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,16 +63,46 @@ var fight = function (enemyName) {
     }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    for (var i = 0; i < enemyNames.length; i++) {
 
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(enemyNames[i]);
-    } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(enemyNames[i]);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
     }
-}
+
+    // play again
+    endGame();
+};
+
+var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("You've lost your robot in battle.")
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Play again soon!");
+    }
+};
+
+startGame();
