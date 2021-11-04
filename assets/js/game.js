579e40bba3aa6318976347753408e830c00a56b9
JVesterfelt <81572838+jvesterfelt@users.noreply.github.com>
2021-11-03 18:06:58 -0600

    Fixed bug with prompting for name

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    // Loop goes here
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    "name": window.prompt("What is your robot's name?"),
+    "name": getPlayerName(),
     "health": 100,
     "attack": 10,
     "money": 10,
@@ -143,7 +155,8 @@ var startGame = function() {
 
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    
+            debugger;
+
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
             fight(pickedEnemyObj);
